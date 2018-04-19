11f3575af25616416ad08d4c0f3b67b7051ce015
Sobolvitya <vasobol1995@gmail.com>
2018-04-19 13:54:58 +0200

    refactor general training with BP

--- Diploma/test.py
+++ Diploma/test.py
@@ -1,31 +1,33 @@
 from sklearn.datasets import load_breast_cancer
+from sklearn.metrics import precision_score
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 from sklearn.neural_network import MLPClassifier
-from sklearn.metrics import classification_report,confusion_matrix
+from sklearn.preprocessing import StandardScaler
 
 
-cancer = load_breast_cancer()
-print(cancer.keys())
-X = [each[:3] for each in cancer['data']]
-y = cancer['target']
-X_train, X_test, y_train, y_test = train_test_split(X, y)
-scaler = StandardScaler()
-scaler.fit(X_train)
-X_train = scaler.transform(X_train)
-X_test = scaler.transform(X_test)
-mlp = MLPClassifier(hidden_layer_sizes=(3, 4, 3))
-mlp.fit(X_train, y_train)
-predictions = mlp.predict(X_test)
-print(confusion_matrix(y_test,predictions))
-print(classification_report(y_test,predictions))
+def trainNN():
+    data_set = load_breast_cancer()
+    X = data_set['data']
+    y = data_set['target']
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+    scaler = StandardScaler()
+    scaler.fit(X_train)
+    X_train = scaler.transform(X_train)
+    X_test = scaler.transform(X_test)
+    mlp = MLPClassifier(hidden_layer_sizes=(3, 3, 3))
+    mlp.fit(X_train, y_train)
+    predictions = mlp.predict(X_test)
+    return precision_score(y_test, predictions)
 
-print("==================NN TOPOLOGY=============")
-for i in range(0, len(mlp.coefs_)):
-    print("Layer " + str(i))
-    print(mlp.coefs_[i])
+avg = 0
+count = 500
+for i in range(0, count):
+    print("Step: " + str(i))
+    avg += trainNN()
+print("Final result is:")
+print(avg/count)
 
-print(mlp.intercepts_[1])
+
 
 
 
